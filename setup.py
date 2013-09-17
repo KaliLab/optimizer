9e3c840e7b53d87af42201826717e5b5e1c86670
Mike Vella <vellamike@gmail.com>
2013-09-17 02:40:41 +0100

    installer working, no unit tests

--- setup.py
+++ setup.py
@@ -2,14 +2,8 @@
 
 from distutils.core import setup
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-long_description = open("README.md").read()
-
 setup(
-    data_files=[()],
-    install_requires=required,
+#    data_files=[()],
     name = "optimizer",
     version = '0.0.1alpha',
     packages = ['optimizer'],
@@ -17,7 +11,7 @@ setup(
     author = "libNeuroML authors and contributors",
     author_email = "vellamike@gmail.com",
     description = "A Python library for optimization of neuronal models",
-    long_description = long_description,
+#    long_description = long_description,
     license = "BSD",
     url="http://libneuroml.readthedocs.org/en/latest/",
     classifiers = [
