3a69c11d0cc1f2bfd09ff830a43d326563e7f432
pfriedrich <p.friedrich.m@gmail.com>
2013-09-27 10:59:46 +0200

    Voltage clamp fix.

--- optimizer/modelHandler.py
+++ optimizer/modelHandler.py
@@ -382,7 +382,7 @@ class modelHandlerNeuron():
         vec=self.hoc_obj.Vector()
         if settings[2]=="i" and self.stims[0]=="VClamp":
             #vec.record(getattr(h.cas()(0.5).point_processes()[0],"_ref_i"))
-            vec.record(getattr(self.sections[settings[3]](settings[4]).point_processes()[0].i,"_ref_i"))
+            vec.record(getattr(self.sections[settings[3]](settings[4]).point_processes()[0],"_ref_i"))
         else:
             ref='_ref_'+settings[2]
             vec.record(getattr(self.sections[settings[3]](settings[4]),ref))
