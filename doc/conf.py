a83c0941176cbaaee4ca8981d3e5b1a95445a487
Mike Vella <vellamike@gmail.com>
2013-10-31 04:32:36 +0000

    Update conf.py
    typo

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 
-sys.path.insert(0, os.path.abspath('../neuroml'))
+sys.path.insert(0, os.path.abspath('../optimizer'))
 sys.path.insert(0, os.path.abspath('..'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
