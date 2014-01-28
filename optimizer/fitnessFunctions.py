ddfcd882434fa3207b6f4dd5081878d68c490c54
pfriedrich <p.friedrich.m@gmail.com>
2014-01-28 12:55:34 +0100

    Error is raised only if the target length and model trace length is differ with more than 1.

--- optimizer/fitnessFunctions.py
+++ optimizer/fitnessFunctions.py
@@ -767,7 +767,7 @@ class fF():
                 if self.option.output_level == "1":
                     print features, weigths
                 for f, w in zip(features, weigths):
-                    if len(self.model.record[0])!=len(self.reader.data.GetTrace(k)):
+                    if abs(len(self.model.record[0])-len(self.reader.data.GetTrace(k)))>1:
                         raise sizeError("model: " + str(len(self.model.record[0])) + ", target: " + str(len(self.reader.data.GetTrace(k))))
                     temp_fit += w * (f(self.model.record[0],
                                                     self.reader.data.GetTrace(k), args))
