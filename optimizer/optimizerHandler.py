c47fba3c8c1d2fa9411e6f2751615bbdcea46a3b
root <mohacsimate91@gmail.com>
2020-04-27 20:56:55 +0200

    rand

--- optimizer/optimizerHandler.py
+++ optimizer/optimizerHandler.py
@@ -1120,7 +1120,7 @@ class Random_Search_Inspyred(baseOptimizer):
 
 			try:
 				pool = multiprocessing.Pool(processes=int(self.number_of_cpu))
-				results = [pool.apply_async(self.ffun, ([c], pickled_args)) for c in act_candidate]
+				results = [pool.apply(self.ffun, ([c], pickled_args)) for c in act_candidate]
 				pool.close()
 				pool.join()
 				act_fitess=[r.get()[0] for r in results]
