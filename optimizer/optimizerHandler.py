d37e57d134fa33b7e2a673f95484342117a1ca1c
root <mohacsimate91@gmail.com>
2020-10-08 14:10:11 +0200

    Luca pygmo mp

--- optimizer/optimizerHandler.py
+++ optimizer/optimizerHandler.py
@@ -297,9 +297,9 @@ class PygmoAlgorithmBasis(baseOptimizer):
 		self.boundaries = option_obj.boundaries
 		self.base_dir = option_obj.base_dir
 		if self.option_obj.type[-1]!="features":
-				self.number_of_traces=reader_obj.number_of_traces()
-			else:
-				self.number_of_traces=len(reader_obj.features_data["stim_amp"])
+			self.number_of_traces=reader_obj.number_of_traces()
+		else:
+			self.number_of_traces=len(reader_obj.features_data["stim_amp"])
 		self.n_obj=len(option_obj.GetFitnessParam()[-1])*int(self.number_of_traces)
 		self.num_islands = int(option_obj.num_islands)
 
